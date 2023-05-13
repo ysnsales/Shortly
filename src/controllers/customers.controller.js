eee5c11d0ebd606d5113a75e375f2bc709b366c6
ysnsales <ysnsaless@gmail.com>
2023-05-13 17:50:18 -0300

    Ajustes: conflito de cpf

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -41,7 +41,7 @@ export async function updateCustomer(req, res) {
     const {id} = req.params;
 
     try {
-        const searchCPF = await db.query(`SELECT * FROM customers WHERE cpf = $1;`, [cpf]);
+        const searchCPF = await db.query(`SELECT * FROM customers WHERE cpf = $1 AND id <> $2;`, [cpf, id]);
         if (searchCPF.rows.length > 0) return res.sendStatus(409);
 
         await db.query(`UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`, [name, phone, cpf, birthday, id]);
