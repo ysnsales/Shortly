2b20c21e31d846b2349595f7f1f8778cf1e46794
ysnsales <ysnsaless@gmail.com>
2023-05-13 16:01:47 -0300

    Ajuste CREATE games: resolver conflito de nome ja existente

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -15,7 +15,7 @@ export async function createGames(req, res) {
 
     try {
         const searchGame = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
-        if (searchGame) return res.sendStatus(409)
+        if (searchGame.rows.length > 0) return res.sendStatus(409)
         await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
         res.sendStatus(201)
 
