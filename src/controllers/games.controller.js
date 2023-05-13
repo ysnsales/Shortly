ce9c00f133e3ded7824e9317ba886c2d9f5d26b7
ysnsales <ysnsaless@gmail.com>
2023-05-13 15:21:27 -0300

    GET e CREATE games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,5 +1,25 @@
 import { db } from "../database/database.connection.js";
 
 export async function getGames(req, res) {
-    
+    try {
+        const games = await db.query(`SELECT * FROM games;`)
+        console.table(games.rows)
+        res.send(games.rows)
+    }catch (err) {
+        res.status(500).send(err.message)
+    }
+};
+
+export async function createGames(req, res) {
+    const {name, image, stockTotal, pricePerDay} = req.body;
+
+    try {
+        const searchGame = await db.query(`SELECT * FROM games WHERE name = $1;`, [name]);
+        if (searchGame) return res.sendStatus(409)
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay]);
+        res.sendStatus(201)
+
+    }catch (err) {
+        res.status(500).send(err.message)
+    } 
 }
