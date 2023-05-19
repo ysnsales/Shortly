378bce43e69c1f9cde004bfbfbb44c5926e5e0c1
ysnsales <ysnsaless@gmail.com>
2023-05-19 20:41:52 -0300

    Inserir usuário no banco de dados na rota signup

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,2 +1,18 @@
 import { db } from "../database/database.connection.js";
+import bcrypt from "bcrypt";
 
+export async function signUp (req, res){
+    const {name, email, password} = req.body;
+
+    try {
+        const checkEmail = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+        if (checkEmail.rows.length > 0) return res.sendStatus(409);
+
+        const hashPassword = bcrypt.hashSync(password, 10);
+        await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, hashPassword]);
+        res.sendStatus(201);
+
+    }catch (err){
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { signUpSchema } from "../schemas/auth.schema.js";
+import { signUp } from "../controllers/users.controller.js";
 
 const usersRouter = Router();
+
+usersRouter.post("/signup", validateSchema(signUpSchema), signUp);
 
 export default usersRouter;
