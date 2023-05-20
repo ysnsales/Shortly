50c9071394a8afea79d7c6a414eab67d04c4d07c
ysnsales <ysnsaless@gmail.com>
2023-05-19 21:31:11 -0300

    Login de usuário com token gerado para autenticação, rota signin

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signUp (req, res){
     const {name, email, password} = req.body;
@@ -11,6 +12,26 @@ export async function signUp (req, res){
         const hashPassword = bcrypt.hashSync(password, 10);
         await db.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, hashPassword]);
         res.sendStatus(201);
+
+    }catch (err){
+        res.status(500).send(err.message);
+    }
+};
+
+export async function signIn (req, res){
+    const {email, password} = req.body;
+
+    try {
+        const checkUser = await db.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+        if (checkUser.rows.length === 0) return res.sendStatus(401);
+
+        const user = checkUser.rows[0];
+        const checkPassword = await bcrypt.compare(password, user.password);
+        if (!checkPassword) return res.sendStatus(401);
+
+        const token = uuid();
+        await db.query(`INSERT INTO sessions (user_name, user_id, token) VALUES ($1, $2, $3);`, [user.name, user.id, token]);
+        res.send([user.name, user.id, token])
 
     }catch (err){
         res.status(500).send(err.message);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-import { signUpSchema } from "../schemas/auth.schema.js";
-import { signUp } from "../controllers/users.controller.js";
+import { signInSchema, signUpSchema } from "../schemas/auth.schema.js";
+import { signIn, signUp } from "../controllers/users.controller.js";
 
 const usersRouter = Router();
 
 usersRouter.post("/signup", validateSchema(signUpSchema), signUp);
+usersRouter.post("/signin", validateSchema(signInSchema), signIn);
 
 export default usersRouter;
